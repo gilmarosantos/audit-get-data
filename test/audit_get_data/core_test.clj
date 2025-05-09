191d154c38afb0adea223b225f0481a2ce984a2e
gilmarosantos <gilmarosantos@gmail.com>
2025-05-09 09:24:07 +0400

    First commit

---
+++ test/audit_get_data/core_test.clj
@@ -0,0 +1,7 @@
+(ns audit-get-data.core-test
+  (:require [clojure.test :refer :all]
+            [audit-get-data.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
