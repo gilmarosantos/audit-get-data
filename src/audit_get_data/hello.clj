441c1d802e6ce166c5a83a62a00fbec9b9fd4f3a
gilmarosantos <gilmarosantos@gmail.com>
2025-05-12 08:44:10 +0400

    "API - Hello World With Parameters"

---
+++ src/audit_get_data/hello.clj
@@ -0,0 +1,38 @@
+(ns audit-get-data.hello
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]))
+
+
+(defn respond-hello [request]
+  (let [nm (get-in request [:query-params :name])
+        resp (if (empty? nm)
+               "Hello, world!\n"
+               (str "Hello, " nm "\n"))]
+  {:status 200 :body resp}))
+
+(def routes
+  (route/expand-routes
+    #{["/greet" :get respond-hello :route-name :greet]}))
+
+(def service-map
+    {::http/routes routes
+     ::http/type :jetty
+     ::http/port 8890})
+
+(defn start []
+  (http/start (http/create-server service-map)))
+
+(defonce server (atom nil))
+
+(defn start-dev []
+  (reset! server
+          (http/start (http/create-server
+                        (assoc service-map
+                          ::http/join? false)))))
+
+(defn stop-dev []
+  (http/stop @server))
+
+(defn restart []
+  (stop-dev)
+  (start-dev))
